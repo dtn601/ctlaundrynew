4aac6b05e5d177af21ec66ffad02b9bdd029b94b
Daniel Nguyen <danielnguyen@Daniels-MacBook-Pro.local>
2019-09-18 11:30:48 +0200

    upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,70 +1,114 @@
 
-"use strict";
-// function initMap() {} // now it IS a function and it is in global
-
-//    initMap = function() {
-//     // your code like...
-//     var map = new google.maps.Map(document.getElementById('map'), {
-//             center: {
-//                 lat: -34.397, 
-//                 lng: 150.644
-//             },
-//             zoom: 8});
-//     // and other stuff...
-//   } 
+function initMap() {console.log('map')}
 
 $(function(){
+    console.log('hello')
 
-    console.log('tesing123')
-    $('.content').load('main.html')
-
-    $('body').on('load', function(e){
-      e.preventDefault()
-      console.log('this works?')
-    })
-
-      function callPage(pageRefInput,scroll){
-        $.ajax({
-          url: pageRefInput,
-          type: 'GET',
-          dataType: 'html',
-
-          success: function(res){
-            $('.content').html(res)
-          },
-          error: function(err) {
-            console.log('page not loaded: ', err)
-          },
-          complete: function( xhr, status){
-            goToByScroll(scroll)
-          }
-        })
+     initMap = function() {
+      // your code like... 
+      console.log('map render')
+      var CT = {lat: 30.1929877, lng: -97.7799065}
+      var SC = {lat: 30.366566, lng: -97.694302}
       
-      }
-    function goToByScroll(id){
-        $('html,body').animate({scrollTop: $(id).offset().top - 30}, 'slow')
-      }
+      var CTmap = new google.maps.Map(document.getElementById('CTmap'), {
+              center: CT,
+              zoom: 15,
+              disableDefaultUI: true
+            });
+      var CTMarker = new google.maps.Marker({
+              position: CT,
+              map: CTmap
+            });
+      var CTmap2 = new google.maps.Map(document.getElementById('CTmap2'), {
+              center: CT,
+              zoom: 15,
+              disableDefaultUI: true
+            });
+      var CTMarker2 = new google.maps.Marker({
+              position: CT,
+              map: CTmap2
+            });
+      var SCmap = new google.maps.Map(document.getElementById('SCmap'), {
+              center: SC,
+              zoom: 15,
+              disableDefaultUI: true
+            });
+      var SCMarker = new google.maps.Marker({
+              position: SC,
+              map: SCmap
+            });  
+      var SCmap2 = new google.maps.Map(document.getElementById('SCmap2'), {
+              center: SC,
+              zoom: 15,
+              disableDefaultUI: true
+            });
+      var SCMarker2 = new google.maps.Marker({
+              position: SC,
+              map: SCmap2
+            });  
 
-    $('body').on('click','.links',function(e){
-      e.preventDefault()
-      var test = e.currentTarget.attributes
-        console.log(test)
-        console.log(e.currentTarget.attributes)
-      callPage(test.data.value,test.href.value)
-  });
+    };
+      // and other stuff...
+
+
+    // $('.content').load('main.html', function(e){
+    //    console.log('this works?')
+    //    ajaxComplete()
+
+    // })
+
+    // $('body').on('load', '#header', function(e){
+    //   e.preventDefault()
+    //   console.log('this works?')
+    // })
+
+    //   function callPage(pageRefInput,scroll){
+    //     $.ajax({
+    //       url: pageRefInput,
+    //       type: 'GET',
+    //       dataType: 'html',
+
+    //       success: function(res){
+    //         $('.content').html(res)
+    //       },
+    //       error: function(err) {
+    //         console.log('page not loaded: ', err)
+    //       },
+    //       complete: function( xhr, status){
+    //         ajaxComplete();
+    //         goToByScroll(scroll);
+    //       }
+    //     })
+      
+    //   }
+    // function goToByScroll(id){
+    //     $('html,body').animate({scrollTop: $(id).offset().top - 30}, 'slow')
+    //   }
+
+    // $('body').on('click','.links',function(e){
+    //   e.preventDefault()
+    //   var test = e.currentTarget.attributes
+    //     console.log(test)
+    //     console.log(e.currentTarget.attributes)
+    //   callPage(test.data.value,test.href.value)
+    // });
+
+    $('.sidenav').sidenav();
+    $('.material-box').materialbox();
+    $('.parallax').parallax();
+    $('.tabs').tabs();
+    $('.datepicker').datepicker({
+        disableWeekends: true
+    });
+    $('.tooltipped').tooltip();
+    $('.scrollspy').scrollSpy(); 
+    $('.modal').modal();
 
 
 
 
-        $('.sidenav').sidenav();
-        $('.material-box').materialbox();
-        $('.parallax').parallax();
-        $('.tabs').tabs();
-        $('.datepicker').datepicker({
-            disableWeekends: true
-        });
-        $('.tooltipped').tooltip();
-        $('.scrollspy').scrollSpy(); 
+
+
 
 
     });
